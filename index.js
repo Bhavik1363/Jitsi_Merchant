788ed8961a140fa94b7e1f5e2698b404f40e2ae0
Bhavik1363 <bhavik1363@gmail.com>
2021-03-10 18:34:01 +0530

    Merchant side jitsi completed window notification

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
 /* eslint-disable */
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDQa7jQejd1eAi6Y-knWVuaw3L3nNBLRgY",
-  authDomain: "jitsi-c6b81.firebaseapp.com",
-  projectId: "jitsi-c6b81",
-  storageBucket: "jitsi-c6b81.appspot.com",
-  messagingSenderId: "198792555309",
-  appId: "1:198792555309:web:eb39fbc387daf6d6e2c1a4",
-  measurementId: "G-N5S8X5S0L5"
+  apiKey: 'AIzaSyC9MVIIOcSWDFT30U1s75s5sYdz3dhudpo',
+      authDomain: 'rtc-server.firebaseapp.com',
+      databaseURL: 'https://rtc-server.firebaseio.com',
+      projectId: 'rtc-server',
+      storageBucket: 'rtc-server.appspot.com',
+      messagingSenderId: '38884098845',
+      appId: '1:38884098845:web:8f3f0c021375b4388257f4'
 };
 // Initialize Firebase
 let db = firebase.initializeApp(firebaseConfig);
+
 function GetRoomName() {
   // let room = '';
 
-    let query = db.firestore().collection('Meeting').where("projectId", "==", 120);
-    query.where("attended", "==", false).onSnapshot((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        // res(doc.data().roomName)
-        // Notification Function
-        console.log("Room found", doc.data().roomName);
-        
-        console.log(Notification.permission);
-        if (Notification.permission === "granted") {
-          showNotificatoin(doc.data().roomName);
-        } else if (Notification.permission !== "denied") {
-          Notification.requestPermission().then(permission => {
-            if (permission === "granted") {
-              showNotificatoin(doc.data().roomName);
-            }
-          });
-        }
+  let query = db.firestore().collection('Meeting').where("projectId", "==", "60438790eefc9a00186931bc");
+  query.where("attended", "==", 'start').onSnapshot((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
 
-      })
-    }
-    )
-  
+      console.log("Room found", doc.data().roomName);
+
+      console.log(Notification.permission);
+      if (Notification.permission === "granted") {
+        showNotificatoin(doc.data().roomName);
+      } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(permission => {
+          if (permission === "granted") {
+            showNotificatoin(doc.data().roomName);
+          }
+        });
+      }
+
+    })
+  }
+  )
+
 }
 function showNotificatoin(roomName) {
-  localStorage.setItem('room-name',roomName)
+  localStorage.setItem('room-name', roomName)
   const notification = new Notification("New Meeting", {
-    body: "Your customer waiting in virtual meeting room :"+roomName,
-    requireInteraction : true,
+    body: "Your customer waiting in virtual meeting room :" + roomName,
+    requireInteraction: true,
     dir: "ltr",
     // renotify : true
   })
   notification.onclick = (e) => {
-    db.firestore().collection('Meeting').doc(roomName).set({
-      attended: true,
-    }, { merge: true });
-    console.log(window.location.href);
-    window.open("newtab.html","_blank");
-  
-  };
+    new Promise((res,rej)=>{
+      db.firestore().collection('Meeting').doc(roomName).set({
+      attended : 'Ongoing',
+      },{merge : true})
+      res('clicked')
+    }).then(res=>{
+
+      console.log(window.location.href);
+      JitsiWindow = window.open("newtab.html", "_blank");
+    })
+
+  }
 }
